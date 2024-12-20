4635bf797aa8d03d8d5d415f56d2fa0b955b05e0
Benjamin Cha <mrbrenjamincha@gmail.com>
2024-12-20 10:31:44 -0700

    feat: add Exercise 12 solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  // Calculate age
+  // Find greatest age
+  // Return name
 
+  const ages = people.map(person => {
+    if (person.yearOfDeath) return person.yearOfDeath - person.yearOfBirth;
+    else return 2024 - person.yearOfBirth;
+  });
+  let max = Math.max(...ages);
+  let index = ages.indexOf(max);
+  return people[index];
 };
 
 // Do not edit below this line
