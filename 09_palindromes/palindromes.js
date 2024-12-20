68d2ef04330093a94f79002c36b3cc5650f8a9ff
Benjamin Cha <mrbrenjamincha@gmail.com>
2024-12-20 10:00:56 -0700

    feat: add solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const alphanumeric = "abcdefghijklmnopqrstuvwxyz0123456789";
+  let lowercase = string.toLowerCase();
+  let array = lowercase.split("").filter(char => alphanumeric.includes(char));
+  let filtered = array.join("");
+  let reverse = array.reverse().join("");
+  if (filtered === reverse) return true;
+  else return false;
 };
 
 // Do not edit below this line
