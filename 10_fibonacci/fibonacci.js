db1dfa3cd5e845c8592db68a60879a9ba9c3e21f
Benjamin Cha <mrbrenjamincha@gmail.com>
2024-12-20 10:17:59 -0700

    feat: add Fibonacci exercise solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
+const fibonacci = function(ordinal) {
+  if (ordinal < 0) return "OOPS";
 
+  let sequence = [0, 1];
+  for (let i = 2; i <= ordinal; i++) {
+    sequence[i] = sequence[i - 1] + sequence[i - 2];
+  }
+  return sequence[ordinal];
 };
 
 // Do not edit below this line
