bc20cb98d6d1c03548be976f2f0b6539bd17374d
Benjamin Cha <mrbrenjamincha@gmail.com>
2024-12-20 09:47:37 -0700

    feat: add solutions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, num) => total + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, num) => total * num);
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return Math.pow(x, y);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let result = 1;
+  while (num > 0) {
+    result *= num;
+    num--;
+  }
+  return result;
 };
 
 // Do not edit below this line
