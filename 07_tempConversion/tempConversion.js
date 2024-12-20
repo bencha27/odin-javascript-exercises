3dd7e36dbf30ba80f188ccf3bf4bd494475ec09e
Benjamin Cha <mrbrenjamincha@gmail.com>
2024-12-20 07:27:37 -0700

    feat: add solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let celsius = (temp - 32) * 5/9;
+  if (!Number.isInteger(celsius)) {
+    return Number(celsius.toFixed(1));
+  } else return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let fahrenheit = temp * 9/5 + 32;
+  if (!Number.isInteger(fahrenheit)) {
+    return Number(fahrenheit.toFixed(1));
+  } else return fahrenheit;
 };
 
 // Do not edit below this line
